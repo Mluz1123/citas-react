be5166c0eae78597a36d477f4343863020b26910
HILDEBOG\quinteromf <QuinteroMF>
2023-08-10 09:27:45 -0500

    Primer commit proyecto citas

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({ children }) => {
+  return (
+    <div className='bg-red-600 text-white text-center font-bold p-3 rounded-md mb-5'>
+      {children}
+    </div>
+  )
+}
+
+export default Error
